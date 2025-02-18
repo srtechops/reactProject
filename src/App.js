bacc3272a87a8063684f7ec690fe2ea0b5188964
shanmugaraja <shanmugarajaofficial@gmail.com>
2025-02-18 07:42:19 +0530

    v1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Welcome To SRTechOps Docker Training</h1>
+    </>
+  );
+}
+
+export default App;
